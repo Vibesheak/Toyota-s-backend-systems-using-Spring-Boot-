ff4c418809d5d3084d040ff1daeeb814de7aadb5
Suppiah Vibesheak <116780276+Vibesheak@users.noreply.github.com>
2024-08-18 23:34:49 +0530

    Delete RatingRankingRepository.java

--- RatingRankingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ToyotaWeb.Toyota.Repository;
-
-import com.ToyotaWeb.Toyota.Model.RatingRanking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRankingRepository extends JpaRepository<RatingRanking, Long> {
-}
