3134d8f4e3d78df00c2805b5c32e1fc8375c00f5
Suppiah Vibesheak <116780276+Vibesheak@users.noreply.github.com>
2024-08-18 23:34:08 +0530

    Delete FinancialRepository.java

--- FinancialRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ToyotaWeb.Toyota.Repository;
-
-import com.ToyotaWeb.Toyota.Model.Financial;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FinancialRepository extends JpaRepository<Financial, Long> {
-}
