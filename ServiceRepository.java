bbc09606077ce4c005338b0a6535dd1055019620
Suppiah Vibesheak <116780276+Vibesheak@users.noreply.github.com>
2024-08-18 23:36:52 +0530

    Delete ServiceRepository.java

--- ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ToyotaWeb.Toyota.Repository;
-
-import com.ToyotaWeb.Toyota.Model.ServiceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<ServiceEntity, Long> {
-}
