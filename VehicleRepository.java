b224c06719c747eb6e87f1b715dc4f7df4da8426
Suppiah Vibesheak <116780276+Vibesheak@users.noreply.github.com>
2024-08-18 23:36:44 +0530

    Delete VehicleRepository.java

--- VehicleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ToyotaWeb.Toyota.Repository;
-
-import com.ToyotaWeb.Toyota.Model.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-}
